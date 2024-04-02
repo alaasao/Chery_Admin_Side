a9dfa48cb3bfaccd6d2a9d84ff8dd97bc464941c
alaasao <a_souchi@estin.dz>
2024-04-02 15:49:36 +0100

    vercel file

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,7 +6,7 @@ import SideBar from "./SideBar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <div className={"grid lg:grid-cols-[320px_1fr] "}>
+    <div className={"grid lg:grid-cols-[320px_1fr] poppins"}>
       <SideBar />
       <App />
     </div>
