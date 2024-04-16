0b5446097d28171869edfbac3d2a5e1abdc25edc
alaasao <a_souchi@estin.dz>
2024-04-16 18:01:46 +0100

    signin small screen

--- src/Routes/signin/SignIn.tsx
+++ src/Routes/signin/SignIn.tsx
@@ -6,14 +6,15 @@ const SignIn = () => {
     const [rester,setRester]=useState(false)
   return (
     <div className='flex w-screen'>
-          <img src="../assets/signin.png" alt=""  className='w-[60%] h-screen max-md:hidden' />
-          <div className='flex flex-col items-center w-[40%] max-md:w-full'>
-              <h1 className='capitalize good text-[#D12621] text-4xl mt-[110px] '>bienvenue</h1>
-              <h3 className='text-3xl mt-[120px] max-lg:text-xl'>Veuillez entrer vos coordonnées :</h3>
-              <input type="text" className=' flex w-[80%] h-[71px] border-2 border-black rounded-lg mt-[67px] pl-[20px] text-2xl' placeholder='Entrez votre adresse e-mail' />
+          <img src="../assets/signin.png" alt=""  className='w-[50%] h-screen max-md:hidden' />
+          <div className='flex flex-col items-center w-[50%] max-md:w-full max-md:justify-between max-md:h-screen max-md:py-[100px]'>
+              <h1 className='capitalize good text-[#D12621] text-4xl mt-[110px] max-md:mt-0 '>bienvenue</h1>
+              <h3 className='text-3xl mt-[120px] max-sm:mt-[0px] max-lg:text-xl'>Veuillez entrer vos coordonnées :</h3>
+          <div className='flex flex-col items-center w-full max-md:gap-[67px]'> 
+              <input type="text" className=' flex w-[80%] h-[71px] border-2 border-black rounded-lg mt-[67px] max-sm:mt-[0px] pl-[20px] text-2xl' placeholder='Entrez votre adresse e-mail' />
               
-              <input type="text" className=' flex w-[80%] h-[71px] border-2 border-black rounded-lg mt-[67px] pl-[20px] text-2xl' placeholder='Entrez votre mot de passe' />
-              <div className='mt-[67px] flex items-center text-2xl gap-[20px] font-bold w-[80%] pl-[20px] cursor-pointer' onClick={()=>setRester(prev=>!prev)}>
+              <input type="text" className=' flex w-[80%] h-[71px] border-2 border-black rounded-lg mt-[67px] max-sm:mt-[0px] pl-[20px] text-2xl' placeholder='Entrez votre mot de passe' />
+              </div>  <div className='mt-[67px] max-sm:mt-[0px] flex items-center text-2xl gap-[20px] font-bold w-[80%] pl-[20px] cursor-pointer' onClick={()=>setRester(prev=>!prev)}>
           
                 { rester ? (
                   <div
@@ -32,7 +33,7 @@ const SignIn = () => {
                   )}
                   <div>Restez connecté</div>
               </div>
-              <div className='w-[80%] h-[71px] bg-black text-white mt-[93px]  rounded-lg flex justify-center items-center text-3xl '>Se connecter</div>
+              <div className='w-[80%] h-[71px] bg-black text-white mt-[93px] max-sm:mt-[0px] rounded-lg flex justify-center items-center text-3xl '>Se connecter</div>
               <a href="" className='mt-[100px] underline text-xl'>Mot de passe oublié?</a>
           </div>
     </div>
