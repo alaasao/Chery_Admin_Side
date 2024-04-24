78632d5e2e4b74fe5efa63b0dd8277447898c7a5
alaasao <a_souchi@estin.dz>
2024-04-24 07:32:53 +0100

    little erros

--- src/Routes/cars/components/AddCar.tsx
+++ src/Routes/cars/components/AddCar.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+
 import Car from './Car';
 import {CarModel} from './data';
 
 const AddCar = () => {
-    // useEffect(() => {
-    //     async function fetchData() {
-    //       const response = await axios.get(`https://axeiny.tech:4004/car/${real_id}`);
-    //       setCar(response.data);
-    //       console.log(response.data);
-    //       // ...
-    //     }
-    //     fetchData();
-    //   }, [real_id]);
+
   return (
     <div>
       <Car carDefault={CarModel} readOnly={false} />
