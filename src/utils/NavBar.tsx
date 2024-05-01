9deb413f848d3cedddc5ca93c123e84dcb69e6e1
alaasao <a_souchi@estin.dz>
2024-05-01 04:18:11 +0100

    some event errors

--- src/utils/NavBar.tsx
+++ src/utils/NavBar.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 import { FaRegBell, FaRegUser } from "react-icons/fa";
@@ -5,6 +6,7 @@ import { useLocation } from "react-router-dom";
 const NavBar = () => {
   const location = useLocation();
   const { pathname } = location;
+  console.log(pathname)
   return (
     <div className="w-full h-[103px] flex justify-between items-center px-[37px] shadow-lg max-lg:pl-[50px] ">
       <div className="text-4xl font-good text-[#D12621] max-sm:text-xl"></div>
