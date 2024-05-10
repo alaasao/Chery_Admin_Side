d21d3031f40503056e19e3f20e8777714e60cfae
yacine <a_zeraibi@estin.dz>
2024-05-10 22:04:59 +0100

    add firebase upload

--- src/config/firebase/Upload_Images.jsx
+++ src/config/firebase/Upload_Images.jsx
@@ -1,30 +1,31 @@
-async function uploadImages(files) {
-    if (!Array.isArray(files)) {
-      files = [files];
-    }
-  
-    const uploadPromises = files.map(file => {
-      const storageRef = ref(storage, 'images/' + file.name);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-  
-      return new Promise((resolve, reject) => {
-        uploadTask.on('state_changed', 
-          (snapshot) => {
-            // Handle the upload progress
-          }, 
-          (error) => {
-            // Handle unsuccessful uploads
-            reject(error);
-          }, 
-          () => {
-            // Handle successful uploads
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              resolve(downloadURL);
-            });
-          }
-        );
-      });
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { storage } from "./config";
+import { toastFNC } from "../toast";
+
+export async function uploadImages(files) {
+
+    const uploadPromises = files.map(async file => {
+    const storageRef = ref(storage, 'images/' + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    return new Promise((resolve, reject) => {
+      uploadTask.on('state_changed',
+        (snapshot) => {
+            console.log('uploading')
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+          reject(error);
+        },
+        () => {
+          // Handle successful uploads
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
     });
-  
-    return Promise.all(uploadPromises);
-  }
+  });
+
+  return Promise.all(uploadPromises);
+}
